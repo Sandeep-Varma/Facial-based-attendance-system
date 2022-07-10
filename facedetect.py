065c8aa18646913158ffd42625dfa1525670df67
Sandeep-Varma <dsv6799@gmail.com>
2022-07-10 11:36:33 +0530

    Commit17

--- facedetect.py
+++ facedetect.py
@@ -1,6 +1,9 @@
+import cv2
 from studentslist import update_students_list
 from paths import input_dataset_path
 
-def face_detect_n_crop():
+def face_detect_n_crop(file_path,new_file_path):
+	img = cv2.imread()
+
+def create_training_data():
 	update_students_list()
-	
